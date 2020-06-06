34f95259bbc7750268ae78ad41ed55a6aebd7514
renanmatos-hub <renan.onha@hotmail.com>
2020-06-06 00:55:13 +0000

    Feito o CRUD

--- src/App.js
+++ src/App.js
@@ -1,43 +1,100 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import api from './api';
-import {Link} from 'react-router-dom';
-import Table from '@material-ui/core/Table';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import {
+    makeStyles,
+    AppBar,
+    Toolbar,
+    IconButton, 
+    Typography, 
+    Table, 
+    TableRow, 
+    TableCell, 
+    Dialog, 
+    Button, 
+    DialogTitle, 
+    DialogContent, 
+    DialogContentText, 
+    TextField, 
+    TableHead,
+    DialogActions
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-
 
 
 function App() {
 
     const[lista, setLista] = useState([]); //imutabilidade
     const[loading, setLoading] = useState(true);
+    const [ open, setOpen ] = useState(false);
+    const [ autor, setAutor ] = useState('');
+    const [ titulo, setTitulo ] = useState('');
+    const [ anopublic, setAnopbublic ] = useState('');
+    const [ checked, setChecked ] = useState(true);
 
-    useEffect(() => { //construtor, executado "uma única vez"
+    function listaLivros(){
         api.get('/livros').then((response) => {
             const itens = response.data;
             setLista(itens);
             setLoading(false);
         })
+    }
+
+    useEffect(() => { //construtor, executado "uma única vez"
+        listaLivros();
     }, [])
+
+    const handleChange = (event) => {
+    setChecked(event.target.checked);
+    };
     
+    const openModal = () => setOpen(true);
+
+    const closeModal = () => setOpen(false);
+    
+        function addLivro(){
+        const name = autor;
+        const title = titulo;
+        const number = anopublic;
+
+        api.post('/livros', {autor:name, titulo:title, anopublic:number}).then((response) => {
+            setAutor('');
+            setTitulo('');
+            setAnopbublic('');
+            setChecked(true);
+            setOpen(false);
+            listaLivros();
+        });
+    }
+
+    function deletarLivro(id){
+        api.delete(`/livros/${id}`).then((response) => {
+            listaLivros();
+        });
+    }
+
+    function marcarLido(id,lido){
+        if(lido == true){
+            api.patch(`/livros/${id}/nlido`).then((response) => {
+                listaLivros();
+            });
+        } else {
+            api.patch(`/livros/${id}/lido`).then((response) => {
+                listaLivros();
+            });
+        }
+    }
+
     const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }));
 
 const classes = useStyles();
 
@@ -58,16 +115,91 @@ const classes = useStyles();
     </div>
       {loading ? <span>Carregando dados...</span>: <div/>}
       <Table>
+            <TableHead>
+                <TableRow>
+                <   TableCell> <b>Autor</b> </TableCell>
+                    <TableCell> <b>Titulo</b> </TableCell>
+                    <TableCell> <b>Ano</b> </TableCell>
+                    <TableCell> <b>Lido</b> </TableCell>
+                    <TableCell> <b>Não Quero Mais Ler</b> </TableCell>
+                </TableRow>
+            </TableHead>
           {lista.map(item => (
             <TableRow key={item.id}>
               <TableCell>{item.autor}</TableCell>
               <TableCell>{item.titulo}</TableCell>
-              <TableCell><input type="checkbox" checked={item.lido}/></TableCell>
+              <TableCell>{item.anopublic}</TableCell>
+              <TableCell>
+                    <input 
+                        type="checkbox" 
+                        onChange={() => marcarLido(item.id,item.lido)}
+                        checked={item.lido == true ? true : false}/>
+              </TableCell>
+              <TableCell>
+                    <Button 
+                        onClick={() => deletarLivro(item.id)}
+                        variant="outlined" 
+                        size="small" 
+                        color="secondary">Deletar</Button>
+                    </TableCell>
           </TableRow> 
           ))}     
       </Table>
-      <Button color="inherit"><Link to="/create">Adicionar</Link></Button>
-      
+    <Button 
+        onClick={openModal}
+        variant="contained" 
+        color="primary" 
+        style={{marginTop: '20px'}}>
+        Adicionar
+    </Button>
+      <Dialog open={open} onClose={closeModal} fullWidth={true} maxWidth="sm">
+            <DialogTitle id="form-dialog-title">Novo Livro</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Qual é o seu livro novo?
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="autor"
+                    label="Nome do Autor"
+                    autoComplete="off"
+                    type="text"
+                    fullWidth
+                    value={autor}
+                    onChange={e => setAutor(e.target.value)}
+                />
+                <TextField
+                    margin="dense"
+                    id="titulo"
+                    label="Título"
+                    autoComplete="off"
+                    type="text"
+                    fullWidth
+                    value={titulo}
+                    onChange={e => setTitulo(e.target.value)}
+
+                />
+                <TextField
+                    margin="dense"
+                    id="anoPublic"
+                    label="Ano de Publicação"
+                    autoComplete="off"
+                    type="year"
+                    fullWidth
+                    value={anopublic}
+                    onChange={e => setAnopbublic(e.target.value)}
+                />                
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={closeModal} color="primary">
+                    Cancelar
+                </Button>
+                <Button color="primary" onClick={addLivro}>
+                    Salvar
+                </Button>
+            </DialogActions>
+         </Dialog>
     </>
   )
 }
